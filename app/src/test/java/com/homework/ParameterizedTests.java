aab4f3c0843e3e6d61065c7ac34980477eaf9890
semen-nenashev <nenashev.sd@gmail.com>
2021-02-12 21:37:46 +0200

    homework done

---
+++ app/src/test/java/com/homework/ParameterizedTests.java
@@ -0,0 +1,56 @@
+package com.homework;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class ParameterizedTests {
+    @DataProvider(name = "testOne")
+    public Object[][] newData() {
+        return new Object[][] {
+                { "Cedric", new Integer(36) },
+                { "Anne", new Integer(37)},
+                {"Sem", new Integer(22)}
+        };
+    }
+
+    @DataProvider(name = "testTwo")
+    public Object[][] Data() {
+        return new Object[][] {
+                {2, 4},
+                {3, 9},
+                {4, 16},
+                {5, 25},
+                {6, 36}
+        };
+    }
+
+    @Test(dataProvider = "testOne")
+    public void PrintName(String name, int age){
+        System.out.println(name + ": "+ age);
+    }
+
+    @Test(dataProvider = "testTwo")
+    public void SquareNumb (int number, int result){
+        int res = number*number;
+        Assert.assertEquals(res,result,"Test failed!");
+    }
+
+
+    @Parameters({ "firstParameter" })
+    @Test
+    public void FirstXMLParam(String firstParameter) {
+        System.out.println("Simple String parameter " + firstParameter);
+    }
+
+    @Parameters({ "secondParameter" })
+    @Test
+    public void SecondXMLParam(String secondParameter) {
+        System.out.println("Second Parameter: " + secondParameter);
+    }
+
+
+
+
+}
---
+++ app/src/test/java/com/homework/SeveralTestGroup.java
@@ -0,0 +1,51 @@
+package com.homework;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class SeveralTestGroup {
+
+    @BeforeGroups
+    public void setUp(){
+        System.out.println("Group tests started!");
+    }
+    @AfterGroups
+    public void tearDown(){
+        System.out.println("Group tests end!");
+    }
+    @Test(groups = {"First group"})
+    public void FirstTest() {
+        Assert.assertEquals(1,1,"Test 1 failed!");
+        System.out.println("Test 1 group 1!");
+    }
+
+    @Test(groups = {"Second group"})
+    public void SecondTest(){
+        Assert.assertEquals(2,2,"Test 2 failed!");
+        System.out.println("Test 2 group 2!");
+    }
+
+    @Test(groups = {"First group"})
+    public void ThirdTest(){
+        Assert.assertEquals(3,3,"Test 3 failed!");
+        System.out.println("Test 3 group 1!");
+    }
+
+    @Test(groups = {"Second group"})
+    public void FourthTest(){
+        Assert.assertEquals(3,3,"Test 4 failed!");
+        System.out.println("Test 4 group 2!");
+    }
+
+    @Test(groups = {"Third group"},dependsOnGroups = {"First group"})
+    public void FifthTest(){
+        Assert.assertEquals(3,3,"Test 5 failed!");
+        System.out.println("Test 5 group 3!");
+    }
+
+    @Test(groups = {"Third group"},dependsOnGroups = {"First group"})
+    public void SixthTest(){
+        Assert.assertEquals(3,3,"Test 6 failed!");
+        System.out.println("Test 6 group 3!");
+    }
+}
